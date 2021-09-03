76151c76f7c39e04901ac250db96937bb6090895
Silvestre <silvestrevideoproducoes@gmail.com>
2021-09-03 13:25:34 -0300

    sistema de gastos familiar

---
+++ gastos.js
@@ -0,0 +1,48 @@
+/*Sistemas de gastos familiar
+
+Crie um objeto que possuirá 2 propriedades, ambas do tipo array:
+
+*receitas:[]
+*despesas:[]
+
+Agora, crie uma função que irá calcular o total de receitas e despesas e irá mostrar um mensagem se a família está com saldo positivo ou negativo, seguido do valor do saldo.
+*/
+
+let family = {
+  incomes: [2500, 3200, 250.43, 360.45],
+  expenses : [320.34, 128.45, 176.87, 1450.00, 20000]
+} 
+
+function sum(array) {
+  let total = 0
+
+  for(let value of array) {
+    total += value
+  }
+
+
+  return total
+}
+
+
+
+function calculateBalance() {
+  const calculateIncomes = sum(family.incomes)
+  const calculateExpenses = sum(family.expenses)
+
+  const total = calculateIncomes - calculateExpenses
+
+  const itsOk = total >= 0
+
+  let balanceText = "Negativo"
+  
+  if (itsOk) {
+    balanceText = "positivo"
+
+  }
+
+  console.log(`Seu saldo é ${balanceText}: ${total.toFixed(2)}R$`)
+
+}
+
+calculateBalance()
